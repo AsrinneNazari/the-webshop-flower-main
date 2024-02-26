fc90a0e9debbbbddeda414f128ca28211f2029f4
Asrinne Nazari <asse_89@hotmail.com>
2024-02-26 08:31:04 +0100

    test

--- src/ts/modules/flowerObjects.ts
+++ src/ts/modules/flowerObjects.ts
@@ -1,7 +1,7 @@
 import { Flowers } from "./flowersClass";
 
 export const flower1 = new Flowers(
-  "/src/img/flower1.png",
+  "./img/flower1.png",
   "Yellow tulips",
   "A bouquet of yellow tulips",
   "A bouquet of yellow tulips is a stunning and vibrant display that exudes warmth, joy, and elegance. Each tulip is characterized by its graceful, slender stem and a beautifully cup-shaped blossom. The radiant yellow hue of the petals is reminiscent of sunshine, symbolizing happiness, positivity, and friendship.",
@@ -9,7 +9,7 @@ export const flower1 = new Flowers(
   1
 );
 export const flower2 = new Flowers(
-  "/src/img/flower2.png",
+  "/the-webshop-flower-main/img/flower2.png",
   "Red tulips",
   "A bouquet of red tulips",
   "A bouquet of red tulips is a striking and passionate arrangement that captivates with its bold and vibrant beauty. Each tulip in the bouquet boasts a long, slender stem that supports a stunning cup-shaped blossom with velvety red petals. The rich and intense red color of the tulips conveys a sense of deep love, romance, and affection.",
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
-import { defineConfig } from "vite";
+/* import { defineConfig } from "vite";
 
 export default defineConfig({
   base: '/the-webshop-flower-main/',
-});
+},
+)
+"include": ["src"]
+
+ */
+
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "module": "ESNext",
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "skipLibCheck": true,
+
+    /* Bundler mode /
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+
+    / Linting */
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": ["src"]
+}
+
 
