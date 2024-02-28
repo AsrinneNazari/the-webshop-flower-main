629b4ec4973a83aad04399a713428922ba9bcbd7
Asrinne Nazari <asse_89@hotmail.com>
2024-02-28 10:56:04 +0100

    fixed my pics

--- src/ts/modules/flowerObjects.ts
+++ src/ts/modules/flowerObjects.ts
@@ -9,7 +9,7 @@ export const flower1 = new Flowers(
   1
 );
 export const flower2 = new Flowers(
-  "src/img/flower2.jpg",
+  "./flower2.jpg",
   "Red tulips",
   "A bouquet of red tulips",
   "A bouquet of red tulips is a striking and passionate arrangement that captivates with its bold and vibrant beauty. Each tulip in the bouquet boasts a long, slender stem that supports a stunning cup-shaped blossom with velvety red petals. The rich and intense red color of the tulips conveys a sense of deep love, romance, and affection.",
@@ -17,7 +17,7 @@ export const flower2 = new Flowers(
   2
 );
 export const flower3 = new Flowers(
-  "the-webshop-flower-main/src/img/flower3.jpg",
+  "./flower3.jpg",
   "Mixed bouquet",
   "A romantic bouquet",
   "A visually enchanting arrangement of flowers meticulously chosen to convey feelings of love, passion, and affection. In this particular bouquet, the combination of various blooms creates a symphony of colors, shapes, and scents that captivate the senses.",
@@ -25,7 +25,7 @@ export const flower3 = new Flowers(
   3
 );
 export const flower4 = new Flowers(
-  "/src/img/flower4.png",
+  "./flower4.png",
   "Pink roses",
   "A bouquet of pink roses",
   "Soft pink roses are like whispers of affection and tenderness. Their delicate hue suggests sweetness and grace. Picture petals unfolding in layers, each one a gentle stroke of blush. These roses are reminiscent of a subtle, blooming romance, expressing admiration, gratitude, and admiration.",
@@ -33,7 +33,7 @@ export const flower4 = new Flowers(
   4
 );
 export const flower5 = new Flowers(
-  "/src/img/flower5.png",
+  "./flower5.png",
   "Red roses",
   "A bouquet of red roses",
   "Red roses are timeless symbols of love, passion, and romance. Their striking and vibrant appearance captures the essence of deep emotions.",
@@ -41,7 +41,7 @@ export const flower5 = new Flowers(
   5
 );
 export const flower6 = new Flowers(
-  "/src/img/flower6.png",
+  "./flower6.png",
   "Wedding bouquet",
   "A wedding bouquet",
   "White roses, with their pristine and pure appearance, often take center stage in a wedding bouquet. Their elegant blooms symbolize innocence and purity, representing the beginning of a new chapter in the bride's life.",
